122f6a31d67bf1c83ae3f1bbb03cd409c030d195
Eddie Franco <eddiefranco626@gmail.com>
2018-12-04 23:48:45 -0600

    working post to sqlite database

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -3,11 +3,12 @@ module.exports = {
   db: {
     database: process.env.DB_NAME || 'songtracker',
     user: process.env.DB_USER || 'songtracker',
-    password: process.env.DB_PASS || 'edbot',
+    password: process.env.DB_PASS || 'songtracker',
     options: {
-      dialetct: process.env.DIALECT || 'sqlite',
+      dialect: process.env.DIALECT || 'sqlite',
       host: process.env.HOST || 'localhost',
-      storage: './songtracker.sqlite'
+      storage: './songtracker.sqlite',
+      operatorsAliases: false
     }
   }
 }
--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 const Sequelize = require('sequelize')
-const config = require('..config/config')
+const config = require('../config/config')
 const db = {}
 
 const sequelize = new Sequelize(
@@ -11,8 +11,11 @@ const sequelize = new Sequelize(
   config.db.options
 )
 
-fs.readdirSync(__dirname)
-  .filter((file) => file !== 'index.js')
+fs
+  .readdirSync(__dirname)
+  .filter((file) =>
+    file !== 'index.js'
+  )
   .forEach((file) => {
     const model = sequelize.import(path.join(__dirname, file))
     db[model.name] = model
