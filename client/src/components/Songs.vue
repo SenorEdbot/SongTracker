2167d0319d6dcc32219183f75cd8982ea5161455
Eddie Franco <eddiefranco626@gmail.com>
2018-12-06 18:41:28 -0600

    working view button takes you to the proper song page

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -14,13 +14,32 @@
           middle>
         <v-icon>add</v-icon>
         </v-btn>
-        <div
-          v-for="song in songs"
-          :key="song.id">
-          {{song.title}} |
-          {{song.artist}} |
-          {{song.genre}} |
-          {{song.album}}
+        <div v-for="song in songs" :key="song.id" class="song">
+          <v-layout>
+            <v-flex xs6>
+              <div class="song-title">
+                {{song.title}}
+              </div>
+              <div class="song-artist">
+                {{song.artist}}
+              </div>
+              <div class="song-genre">
+                {{song.genre}}
+              </div>
+              <v-btn
+                class="cyan"
+                dark
+                @click="navigateTo({
+                  name: 'song',
+                  params: {
+                    songId: song.id
+                  }
+                })">View</v-btn>
+            </v-flex>
+            <v-flex xs6>
+              <img class="album-image" :src="song.albumImage" />
+            </v-flex>
+          </v-layout>
         </div>
       </panel>
     </v-flex>
@@ -35,6 +54,11 @@ export default {
       songs: null
     }
   },
+  methods: {
+    navigateTo (route) {
+      this.$router.push(route)
+    }
+  },
   async mounted () {
     this.songs = (await SongsService.index()).data
   }
@@ -42,5 +66,25 @@ export default {
 </script>
 
 <style scoped>
+.song {
+  padding: 20px;
+  height: 330px;
+  overflow: hidden;
+}
+.song-title {
+  font-size: 30px;
+}
 
+.song-artist {
+font-size: 24px;
+}
+
+.song-genre {
+  font-size: 18px;
+}
+
+.album-image {
+  width: 75%;
+  margin: 0 auto;
+}
 </style>
