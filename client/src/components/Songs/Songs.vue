b4b825e144a1d17df018a6d91dbc24a7c5800b84
Eddie Franco <eddiefranco626@gmail.com>
2018-12-09 00:17:34 -0600

    working search bar

---
+++ client/src/components/Songs/Songs.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-layout>
+    <v-flex xs6 offset-xs3>
+      <songs-search-panel />
+      <songs-panel class="mt-2" />
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import SongsPanel from './SongsPanel'
+import SongsSearchPanel from './SongsSearchPanel'
+import SongsService from '@/services/SongsService'
+export default {
+  components: {
+    SongsPanel,
+    SongsSearchPanel
+  },
+  data () {
+    return {
+      songs: null
+    }
+  },
+  async mounted () {
+    this.songs = (await SongsService.index()).data
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
